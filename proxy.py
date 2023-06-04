7d04815b0e50e2f0b00d9f1f97ba58325b5ad33e
inmove <lisper.inmove@gmail.com>
2023-06-04 11:20:24 +0800

    add more info when query trade

--- proxy.py
+++ proxy.py
@@ -36,4 +36,6 @@ class PaymentProxy:
         resp = QueryResult()
         resp.success = result.get("alipay_trade_query_response").get("code") == "10000"
         resp.third_party_id = result.get("alipay_trade_query_response").get("trade_no")
+        resp.msg = result.get("alipay_trade_query_response").get("msg")
+        resp.sub_msg = result.get("alipay_trade_query_response").get("sub_msg", "")
         return resp
